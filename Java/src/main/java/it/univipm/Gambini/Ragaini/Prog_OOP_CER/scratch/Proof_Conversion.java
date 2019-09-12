31fa7d7b2c2616be550cb2d38f09eee0fcb28e07
DavideRagaini <ragainidavide@gmail.com>
2019-09-12 13:20:06 +0200

    replaced Gson to ClassTo

--- Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/scratch/Proof_Conversion.java
+++ Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/scratch/Proof_Conversion.java
@@ -3,31 +3,29 @@ package it.univipm.Gambini.Ragaini.Prog_OOP_CER.scratch;
 import java.lang.reflect.Field;
 import java.util.Vector;
 
-import com.google.gson.Gson;
-
 import it.univipm.Gambini.Ragaini.Prog_OOP_CER.model.Metadata;
+import it.univipm.Gambini.Ragaini.Prog_OOP_CER.utility.ClassTo;
 
 public class Proof_Conversion {
 	
-	public static void  Proof() {
+	public static String  Proof() {
 		Vector<String> appoggio = new Vector<>();
 		Class<Ref_Class> h = Ref_Class.class;
 		Field[] fields = h.getDeclaredFields();
-		Gson gson = new Gson();
 		String name = null;
 		String attribute_type = null;
 		int j = 0;
-		for (Field e: fields) {
+		for (@SuppressWarnings("unused") Field e: fields) {
 			name = fields[j].toString();//.substring(fields[j].toString().lastIndexOf(".")+1, fields[j].toString().length());
 			System.out.println(name);
 			Class<?> type = fields[j].getType();
 			attribute_type = type.toString();//.substring(type.toString().lastIndexOf(".")+1, type.toString().length()).replace(";", "[]");
-			String json = gson.toJson(new Metadata(name, attribute_type));
-			appoggio.add(json);
+			appoggio.add(ClassTo.Json(new Metadata(name, " ", attribute_type)));
 			System.out.println(attribute_type);
 			j++;
 		}
-		System.out.println(appoggio.toString());
+		//System.out.println(appoggio.toString());
+		return appoggio.toString();
 	}
 	
 }
