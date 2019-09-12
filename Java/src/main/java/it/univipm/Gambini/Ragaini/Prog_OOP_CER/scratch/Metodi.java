4dc07835f8d0b59758232d1e2a7d39b0ab820650
DavideRagaini <ragainidavide@gmail.com>
2019-09-12 13:21:22 +0200

    unified name, type and getter_invoker on the same file

--- Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/scratch/Metodi.java
+++ Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/scratch/Metodi.java
@@ -1,34 +1,73 @@
 package it.univipm.Gambini.Ragaini.Prog_OOP_CER.scratch;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
 
 import it.univipm.Gambini.Ragaini.Prog_OOP_CER.model.Header;
+import it.univipm.Gambini.Ragaini.Prog_OOP_CER.model.Metadata;
+import it.univipm.Gambini.Ragaini.Prog_OOP_CER.utility.ClassTo;
 
 public class Metodi {
-		public static String invoke_getter(Header header) {
+	
+	
+	
+	public static Vector<String> invoke_Name_and_Type(Header header) {
+		
+		Vector<String> appoggio = new Vector<>();
+		Class<Header> h = Header.class;
+		Field[] fields = h.getDeclaredFields();
+		
+		ArrayList<?> sf = Metodi.invoke_getter(header);
+		
+		String name = new String();
+		String attribute_type = new String();
+		String json = new String();
+		
+		int j = 0;
+
+		for (@SuppressWarnings("unused") Field e: fields) {
+			name = fields[j].toString().substring(fields[j].toString().lastIndexOf(".")+1, fields[j].toString().length());
 			
-			ArrayList<String> getterResults = new ArrayList<>();
-			try {
-				Class headerClass = Class.forName("it.univipm.Gambini.Ragaini.Prog_OOP_CER.model.Header");
-				Method[] methods = headerClass.getDeclaredMethods();
-				
-				for (Method method : methods) {
-					if (method.getName().startsWith("get")){
-							if(method.getReturnType().isArray()){ 
-								String appoggio = Arrays.deepToString((Object[]) method.invoke(header));
-								getterResults.add(appoggio);}
-							else
-								{getterResults.add(method.invoke(header).toString());
-						}
-						
+			Class<?> type = fields[j].getType();
+			attribute_type = type.toString().substring(type.toString().lastIndexOf(".")+1, type.toString().length()).replace(";", "[]");
+			
+			json = ClassTo.Json(new Metadata(name, sf.toArray()[j].toString(), attribute_type));
+			
+			appoggio.add(json);
+			
+			j++;
+		}
+		
+		return appoggio;
+		
+	}
+	
+	
+	
+	public static ArrayList<String> invoke_getter(Header header) {
+
+		ArrayList<String> getterResults = new ArrayList<>();
+		try {
+			Class<?> headerClass = Class.forName("it.univipm.Gambini.Ragaini.Prog_OOP_CER.model.Header");
+			Method[] methods = headerClass.getDeclaredMethods();
+
+			for (Method method : methods) {
+				if (method.getName().startsWith("get")){
+					if(method.getReturnType().isArray()) { 
+						String appoggio = Arrays.deepToString((Object[]) method.invoke(header));
+						getterResults.add(appoggio);
 					}
+					else {
+						getterResults.add(method.invoke(header).toString());
+					}
+
 				}
-				
-				
-			//Log.d(getterResults.toString());
+			}
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,6 +81,7 @@ public class Metodi {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-			return getterResults.toString();
-		}
+
+		return getterResults;
+	}
 }
