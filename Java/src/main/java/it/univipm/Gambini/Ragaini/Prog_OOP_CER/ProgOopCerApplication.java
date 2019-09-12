00b04ba42b21477c246810dc4f3af12dfad774b8
DavideRagaini <ragainidavide@gmail.com>
2019-09-12 13:25:56 +0200

    cleared from unnecessary

--- Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/ProgOopCerApplication.java
+++ Java/src/main/java/it/univipm/Gambini/Ragaini/Prog_OOP_CER/ProgOopCerApplication.java
@@ -3,8 +3,6 @@ package it.univipm.Gambini.Ragaini.Prog_OOP_CER;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import it.univipm.Gambini.Ragaini.Prog_OOP_CER.scratch.Metodi;
-
 @SpringBootApplication
 public class ProgOopCerApplication {
 
