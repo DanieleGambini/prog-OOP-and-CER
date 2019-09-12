c97a50fdd3373456e680f8a3a85657f85c5a823c
DavideRagaini <ragainidavide@gmail.com>
2019-09-12 19:34:48 +0200

    sostituisce class.py

---
+++ Python/dataClass.py
@@ -0,0 +1,11 @@
+class Data:
+    freq
+    unit
+    objective
+    timePeriod
+    
+    def __init__(self, f, u, obj, tP):
+        self.freq=f
+        self.unit=u
+        self.objective=obj
+        self.timePeriod=tP
